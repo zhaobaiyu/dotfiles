acebce7b62b2c826c9dbb5acc708ad80a7532470
Baiyu Zhao <csbaiyu@gmail.com>
2016-03-23 15:48:06 +0800

    init emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,20 @@
+(setq read-file-name-completion-ignore-case 'non-nil)
+(setq initial-frame-alist '((top . 0) (left . 0)   (width . 147) (height . 34)))
+(setq default-frame-alist '((top . 0) (right . 0)   (width .80) (height . 34))) 
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
