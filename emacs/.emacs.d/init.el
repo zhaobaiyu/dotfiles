efb258a8871ec5576d1b5060fb5e90552856bb7f
Baiyu Zhao <csbaiyu@gmail.com>
2016-06-01 10:01:17 +0800

    init rime & add linum to emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,12 +1,18 @@
 (setq read-file-name-completion-ignore-case 'non-nil)
  ;; (setq initial-frame-alist '((top . 0) (left . 0)   (width . 147) (height . 34)))
 (custom-set-variables '(initial-frame-alist (quote ((fullscreen . maximized)))))
-(setq default-frame-alist '((top . 0) (right . 0)   (width .80) (height . 34))) 
+(setq default-frame-alist '((top . 0) (right . 0)   (width .80) (height . 34)))
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+ ;; linum
+(add-hook 'find-file-hook (lambda () (linum-mode 1)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
