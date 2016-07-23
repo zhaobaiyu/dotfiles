8b81aa027494244dc21d4eba35a06e0e990d436a
Baiyu Zhao <csbaiyu@gmail.com>
2016-07-23 14:57:33 +0800

    modify c++ company-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,18 @@
 (setq read-file-name-completion-ignore-case 'non-nil)
  ;; (setq initial-frame-alist '((top . 0) (left . 0)   (width . 147) (height . 34)))
-(custom-set-variables '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-c-headers-path-system
+   (quote
+    ("/usr/include/c++/5/" "/usr/include/" "/usr/local/include/")))
+ '(company-clang-arguments (quote ("-std=c++11")))
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 2)
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
 (setq default-frame-alist '((top . 0) (right . 0)   (width .80) (height . 34)))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
@@ -13,12 +25,7 @@
  ;; linum
 (add-hook 'find-file-hook (lambda () (linum-mode 1)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (manoj-dark))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,3 +40,12 @@
  ;; company-mode
 (autoload 'company-mode "company" nil t)
 (add-hook 'after-init-hook 'global-company-mode)
+
+ ;; melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize) 
+
+ ;; company-c-headers
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-c-headers))
